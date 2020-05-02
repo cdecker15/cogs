bbaf23a2718ab622a64b39d8ff848d3b4d884879
cdecker15 <cdecker15>
2020-05-02 12:34:04 -0700

    Added calc function

--- viswax/viswax.py
+++ viswax/viswax.py
@@ -20,6 +20,77 @@ def pullVis():
     for tag in results.find_all('b'):
         a.append(tag.string)
 
+def calc_cost(rune):
+    amount = 0
+    item_id = 0
+    if rune == "Mind Rune":
+        amount = 2000
+        item_id = 558
+    if rune == "Body Rune":
+        amount = 2000
+        item_id = 559
+    if rune == "Air Rune":
+        amount = 1000
+        item_id = 556
+    if rune == "Water Rune":
+        amount = 1000
+        item_id = 555
+    if rune == "Earth Rune":
+        amount = 1000
+        item_id = 557
+    if rune == "Fire Rune":
+        amount = 1000
+        item_id = 554
+    if rune == "Dust Rune":
+        amount = 500
+        item_id = 4696
+    if rune == "Lava Rune":
+        amount = 500
+        item_id = 4699
+    if rune == "Mist Rune":
+        amount = 500
+        item_id = 4695
+    if rune == "Smoke Rune":
+        amount = 500
+        item_id = 4697
+    if rune == "Steam Rune":
+        amount = 500
+        item_id = 4694
+    if rune == "Chaos Rune":
+        amount = 500
+        item_id = 562
+    if rune == "Cosmic Rune":
+        amount = 400
+        item_id = 564
+    if rune == "Death Rune":
+        amount = 400
+        item_id = 560
+    if rune == "Nature Rune":
+        amount = 350
+        item_id = 561
+    if rune == "Blood Rune":
+        amount = 350
+        item_id = 565
+    if rune == "Mud Rune":
+        amount = 300
+        item_id = 4698
+    if rune == "Law Rune":
+        amount = 300
+        item_id = 563
+    if rune == "Astral Rune":
+        amount = 300
+        item_id = 9075
+    if rune == "Soul Rune":
+        amount = 300
+        item_id = 566
+
+    with request.urlopen(
+            'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + str(item_id)) as response:
+        source = response.read()
+        data = json.loads(source)
+
+    return (data['item']['current']['price']) * amount
+
 
 class Viswax(commands.Cog):
 
@@ -36,12 +107,12 @@ class Viswax(commands.Cog):
         total1 = 0
         total2 = 0
         total3 = 0
-        #total1 += calc_cost(a[0])
-        #total1 += calc_cost(a[1])
-        #total2 += calc_cost(a[0])
-        #total2 += calc_cost(a[2])
-        #total3 += calc_cost(a[0])
-        #total3 += calc_cost(a[3])
+        total1 += calc_cost(a[0])
+        total2 = total1
+        total3 = total1
+        total1 += calc_cost(a[1])
+        total2 += calc_cost(a[2])
+        total3 += calc_cost(a[3])
 
         with request.urlopen(
                 'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=32092') as response:
@@ -55,4 +126,4 @@ class Viswax(commands.Cog):
             viswax_cost = float(viswax_cost) * 1000
 
         viswax_cost = int(viswax_cost) * 100
-        await ctx.send(words)
+        await ctx.send(total1 + "Viswax (100) at today's price: " + str(viswax_cost))
