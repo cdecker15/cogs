11736246458e07a08553e648debd503e005ed60a
unknown <cdecker15>
2020-05-02 10:49:53 -0700

    Added myviswax command

--- viswax/viswax.py
+++ viswax/viswax.py
@@ -3,6 +3,9 @@ from redbot.core import commands
 import requests
 from bs4 import BeautifulSoup
 
+import urllib.request as request
+import json
+
 URL = 'https://warbandtracker.com/goldberg/'
 page = requests.get(URL)
 
@@ -11,7 +14,6 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.table
 
 a = []
-count = 1
 
 for tag in results.find_all('b'):
     a.append(tag.string)
@@ -23,3 +25,11 @@ class Viswax(commands.Cog):
     async def viswax(self, ctx):
         """Checks the daily viswax combinations in Runescape."""
         await ctx.send("First Rune: " + a[0] + "\n" + "Second Rune: " + a[1] + ", " + a[2] + ", " + a[3])
+
+    @commands.command()
+    async def myviswax(self, ctx):
+        """Calculates the cost of your daily viswax combinations in Runescape."""
+        with request.urlopen('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=32092') as response:
+            source = response.read()
+            data = json.loads(source)
+        await ctx.send("Viswax costs " + data['current']['price'])
