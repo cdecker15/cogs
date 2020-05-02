0fe2f389d08fbf992613a6db18acbd3ebb45ab93
Jacob Y <jyarling@sbcglobal.net>
2020-05-01 18:41:44 -0700

    V1

--- viswax/viswax.py
+++ viswax/viswax.py
@@ -1,10 +1,24 @@
 from redbot.core import commands
 
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://warbandtracker.com/goldberg/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.table
+
+a = []
+count = 1
+
+for tag in results.find_all('b'):
+    a.append(tag.string)
+
+
 class Viswax(commands.Cog):
-    """My custom cog"""
 
     @commands.command()
     async def viswax(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("First Rune: " + a[0] + "\n" + "Second Rune: " + a[1] + ", " + a[2] + ", " + a[3])
