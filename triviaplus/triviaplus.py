d00ca6f44597d47531d1fd680852c16afd8e50ed
Jacob Y <jyarling@sbcglobal.net>
2020-05-02 16:39:46 -0700

    Giving it a go

--- triviaplus/triviaplus.py
+++ triviaplus/triviaplus.py
@@ -1,5 +1,6 @@
 from redbot.core import commands
 
+import asyncio
 import urllib.request as request
 import json
 import random
@@ -13,6 +14,25 @@ def un_escape(s):
     s = s.replace("&quot;", "\"")
     s = s.replace("&#039;", "\'")
     return s
+
+def check_answer(self, answer):
+    return True
+
+async def wait_for_answer(self, answer):
+    try:
+        message = await self.ctx.bot.wait_for(
+            "message", check=self.check_answer(answer), timeout=10
+        )
+    except asyncio.TimeoutError:
+        if time.time() - self._last_response >= 10:
+            await self.ctx.send(_("Guys...? Well, I guess I'll stop then."))
+            self.stop()
+            return False
+    else:
+        self.scores[message.author] += 1
+        reply = _("You got it {user}! **+1** to you!").format(user=message.author.display_name)
+        await self.ctx.send(reply)
+    return True
 
 
 class TriviaPlus(commands.Cog):
@@ -38,5 +58,7 @@ class TriviaPlus(commands.Cog):
             for choice in choices:
                 answers_formatted += choice + '\n'
             await ctx.send(question + "\n" + answers_formatted)
-            time.sleep(10)
+            continue_ = await self.wait_for_answer()
             await ctx.send(correct_answer)
+
+        await self.ctx.send(_("There are no more questions!"))
