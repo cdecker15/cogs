0866a5f70b09fab4d5516b70fc0134d29305fa18
cdecker15 <cdecker15>
2020-05-02 13:17:20 -0700

    Fix runescape's problems

--- viswax/viswax.py
+++ viswax/viswax.py
@@ -93,7 +93,7 @@ def calc_cost(rune):
     result = data['item']['current']['price']
 
     if type(result == 'str'):
-        result = result.replace(',', '')
+        result = str(result.replace(',', ''))
         result = int(result)
     return result * amount
 
