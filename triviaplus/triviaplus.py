89ce9d8f1bd97d4d62341e8dbb32ab2782ac4223
Jacob Y <jyarling@sbcglobal.net>
2020-05-02 16:09:13 -0700

    Testing

--- triviaplus/triviaplus.py
+++ triviaplus/triviaplus.py
@@ -5,9 +5,35 @@ import json
 import random
 
 
-class TriviaPlus(commands.Cog):
+def un_escape(s):
+    s = s.replace("&lt;", "<")
+    s = s.replace("&gt;", ">")
+    s = s.replace("&amp;", "&")
+    s = s.replace("&quot;", "\"")
+    s = s.replace("&#039;", "\'")
+    return s
 
+
+class TriviaPlus(commands.Cog):
     @commands.command()
     async def triviaplus(self, ctx):
-        """Checks the daily viswax combinations in Runescape."""
-        await ctx.send("...")
+        """Trivia Plus!"""
+        with request.urlopen('https://opentdb.com/api.php?amount=1') as response:
+            source = response.read()
+            data = json.loads(source)
+
+        data = data['results']
+
+        choices = []
+
+        for result in data:
+            question = un_escape(result['question'])
+            correct_answer = un_escape(result['correct_answer'])
+            choices.append(correct_answer)
+            for answer in result['incorrect_answers']:
+                choices.append(un_escape(answer))
+                random.shuffle(choices)
+            answers_formatted = "";
+            for choice in choices:
+                final += choice + '\n'
+            await ctx.send(question + "\n" + answers_formatted)
